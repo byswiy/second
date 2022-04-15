33c45f8abe15b13af276d50bb526b4f5d8666bcb
DESKTOP-0I8FFQ7\USER <byswiy99@naver.com>
2022-04-15 13:36:45 +0900

    MenuNumber.java : 클래스 멤버 변수 대문자로 수정 Ex04.java : JOIN 메서드화

--- web_3/src/main/java/etc/Ex04.java
+++ web_3/src/main/java/etc/Ex04.java
@@ -8,90 +8,104 @@ import java.sql.Statement;
 import java.util.Scanner;
 
 public class Ex04 {
+	public static int inputMenuNumber() {
+		Scanner scanf = new Scanner(System.in);
+		
+		System.out.println("<< 뫄뫄뫄 프로그램 >>");
+		System.out.println("1. 회원가입");
+		System.out.println("2. 로그인");
+		System.out.println("3. 프로그램 종료");
+		System.out.print("메뉴 선택 -> ");
+		
+		int menu = scanf.nextInt();
+		
+		return menu;
+	}
+	
+	// 메서드는 한 번에 하나의 동작(역할)만 하는게 제일 좋음
+	public static void join() {
+		Scanner scanf = new Scanner(System.in);
+		
+		System.out.println("<< 회원가입 >>");
+		
+		System.out.print("아이디 -> ");
+		String id = scanf.next();
+		
+		System.out.print("비밀번호 -> ");
+		String pw = scanf.next();
+		
+		System.out.print("이름 -> ");
+		String name = scanf.next();
+		
+		Connection conn = null;
+		Statement stmt = null;
+		
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			
+			conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/shopdb?user=root&password=1234");
+		
+			stmt = conn.createStatement();
+			
+			String sql = "INSERT INTO userinfo(id, pw, name) VALUES('" + id + "', '" + pw + "', '" + name + "')";
+//			System.out.println("sql = " + sql);
+			
+			try {
+				// executeUpdate메서드가 return 해주는 것 -> 정수
+			// 정수를 return하는 이유는 쿼리를 실행해서 영향 받은 행을 알려주기 위해
+				int count = stmt.executeUpdate(sql);
+				if(count == 1) {
+					System.out.println("회원 가입 완료");
+				} else {
+					System.out.println("회원 가입 실패");
+				}
+			} catch(SQLIntegrityConstraintViolationException e) {
+				System.out.println("이미 사용중인 아이디입니다");
+			}
+			
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if(stmt != null) {
+				try {
+					stmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			if(conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 
 	public static void main(String[] args) {
-		Scanner scanf = new Scanner(System.in);
+		
 		
 		boolean isRunning = true;
 		
 		while(isRunning) {
-			System.out.println("<< 뫄뫄뫄 프로그램 >>");
-			System.out.println("1. 회원가입");
-			System.out.println("2. 로그인");
-			System.out.println("3. 프로그램 종료");
-			System.out.print("메뉴 선택 -> ");
 			
-			int menu = scanf.nextInt();
+			int menu = inputMenuNumber();
 			
 			switch(menu) {
-			case MenuNumber.join:
-				System.out.println("<< 회원가입 >>");
-				
-				System.out.print("아이디 -> ");
-				String id = scanf.next();
-				
-				System.out.print("비밀번호 -> ");
-				String pw = scanf.next();
-				
-				System.out.print("이름 -> ");
-				String name = scanf.next();
-				
-				Connection conn = null;
-				Statement stmt = null;
-				
-				try {
-					Class.forName("org.mariadb.jdbc.Driver");
-					
-					conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/shopdb?user=root&password=1234");
-				
-					stmt = conn.createStatement();
-					
-					String sql = "INSERT INTO userinfo(id, pw, name) VALUES('" + id + "', '" + pw + "', '" + name + "')";
-//					System.out.println("sql = " + sql);
-					
-					try {
-						// executeUpdate메서드가 return 해주는 것 -> 정수
-					// 정수를 return하는 이유는 쿼리를 실행해서 영향 받은 행을 알려주기 위해
-						int count = stmt.executeUpdate(sql);
-						if(count == 1) {
-							System.out.println("회원 가입 완료");
-						} else {
-							System.out.println("회원 가입 실패");
-						}
-					} catch(SQLIntegrityConstraintViolationException e) {
-						System.out.println("이미 사용중인 아이디입니다");
-					}
-					
-				} catch (ClassNotFoundException | SQLException e) {
-					e.printStackTrace();
-				} finally {
-					if(stmt != null) {
-						try {
-							stmt.close();
-						} catch (SQLException e) {
-							e.printStackTrace();
-						}
-					}
-					
-					if(conn != null) {
-						try {
-							conn.close();
-						} catch (SQLException e) {
-							e.printStackTrace();
-						}
-					}
-				}
-				
-				break;
-			case MenuNumber.login:
-				System.out.println("<< 로그인 >>");
-				break;
-			case MenuNumber.exit:
-				System.out.println("프로그램을 종료합니다");
-				isRunning = false;
-				break;
-			default :
-				System.out.println("번호를 잘못 입력하셨습니다");
+				case MenuNumber.JOIN:
+					join();
+					break;
+				case MenuNumber.LOGIN:
+					System.out.println("<< 로그인 >>");
+					break;
+				case MenuNumber.EXIT:
+					System.out.println("프로그램을 종료합니다");
+					isRunning = false;
+					break;
+				default :
+					System.out.println("번호를 잘못 입력하셨습니다");
 			} // end switch
 			
 		} // end while
--- web_3/src/main/java/etc/MenuNumber.java
+++ web_3/src/main/java/etc/MenuNumber.java
@@ -1,5 +1,10 @@
 package etc;
 
 public class MenuNumber {
-
+	// 클래스 멤버 변수를 활용해서 1번 메뉴의 이름을 생성
+	public static final int JOIN = 1;
+	// 2번 메뉴 이름 -> 로그인
+	public static final int LOGIN = 2;
+	// 3번 메뉴 이름 -> 종료
+	public static final int EXIT = 3;
 }
