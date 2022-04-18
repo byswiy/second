04421ff72b4ffc3d70a526ce7a8397acae125cbc
DESKTOP-0I8FFQ7\USER <byswiy99@naver.com>
2022-04-18 13:12:11 +0900

    Database.java : DB 커넥션을 생성해서 반환하는 메서드

--- web_3/src/main/java/etc/Database.java
+++ web_3/src/main/java/etc/Database.java
@@ -1,5 +1,8 @@
 package etc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,6 +13,21 @@ public class Database {
 	public static List<MemberInfo> memberInfoTable = new ArrayList<>();
 	
 	public static List<NoticeInfo> noticeInfoTable = new ArrayList<>();
+	
+	// DB 커넥션을 생성해서 반환하는 메서드
+	public Connection getConnection() {
+		Connection conn = null;
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			
+			conn = DriverManager.getConnection("jdbc:mariadb://192.168.10.4:3306/shopdb/user=root&password=1234");
+			
+			
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
 	
 }
 
